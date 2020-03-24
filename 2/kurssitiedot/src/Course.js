8a87f573eb2cdd29e63474e21c0e9e6b2f750772
30496491 <j4m4n4@gmail.com>
2020-03-24 22:38:06 +0200

    Finished 2.5

---
+++ 2/kurssitiedot/src/Course.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Course = (props) => {
+    return (
+        <>
+            <Header course={props.course.name} />
+            <Content parts={props.course.parts} />
+            <b><Total parts={props.course.parts} /></b>
+        </>
+    )
+}
+
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+    if (props.parts.length > 0) {
+        return (
+            <>
+                {props.parts.map(part => <Part name={part.name} exercises={part.exercises} key={part.id} />)}
+            </>
+        )
+    } else {
+        return ("No courses are available.")
+    }
+}
+
+const Total = (props) => {
+    const total = props.parts.reduce((s, p) => s + p.exercises, 0)
+    return (
+        <p>Number of exercises {total}</p>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p> {props.name} {props.exercises} </p>
+    )
+}
+
+export default Course
