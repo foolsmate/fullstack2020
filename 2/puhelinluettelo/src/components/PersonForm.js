b3277a919a82538cc136f0cb1f3d6fd6ce5ac5eb
30496491 <j4m4n4@gmail.com>
2020-03-31 17:55:45 +0300

    Finished 2.11

--- 2/puhelinluettelo/src/components/PersonForm.js
+++ 2/puhelinluettelo/src/components/PersonForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const PersonForm = ({ newName, setNewName, newNumber, setNewNumber, setPersons, persons }) => {
     const handleNameChange = (event) => {
