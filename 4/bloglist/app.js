711541e361b60a83ce7a1754f2cdac5782c6fee3
30496491 <j4m4n4@gmail.com>
2020-04-13 19:40:46 +0300

    #2

--- 4/bloglist/app.js
+++ 4/bloglist/app.js
@@ -2,15 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
-  
-  const Blog =  mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blogs')
   
   const mongoUrl = 'mongodb+srv://fullstack:poopoo123@cluster0-icpr3.mongodb.net/bloglist?retryWrites=true&w=majority'
   
@@ -18,23 +10,7 @@ const blogSchema = mongoose.Schema({
   
   app.use(cors())
   app.use(express.json())
-  
-  app.get('/api/blogs', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-  })
-  
-  app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
-  
+
+  app.use('/api/blogs', blogsRouter)  
+
   module.exports = app
--- 4/bloglist/controllers/blogs.js
+++ 4/bloglist/controllers/blogs.js
@@ -1,4 +1,22 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-module.exports = notesRouter
+blogsRouter.get('/', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+module.exports = blogsRouter
